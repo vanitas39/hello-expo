a5fceda726cd1628b8429b17ab686145f2b7b98b
vanitas39 <jhyoo322@naver.com>
2024-01-29 17:14:41 +0900

    day-2-flex-style

--- App.js
+++ App.js
@@ -1,21 +1,70 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>나의 첫번째 앱입니다.</Text>
-      <Text>멋진 앱을 만들꺼에요.</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.item1}></View>
+      <View style={styles.item2}></View>
+      <View style={{flex: 3, flexDirection:'row', backgroundColor: 'gray',gap: 10}}>
+        <View style={styles.item3_1}></View>
+        <View style={styles.item3_2}></View>
+        <View style={styles.item3_3}></View>
+        <View style={styles.item3_4}></View>
+      </View>
+      <View style={styles.item4}></View>
+      <View style={styles.item5}></View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    //flexDirection: 'row',
+    backgroundColor: 'white',
+  },
+
+  item1: {
+    flex: 1,
+    height: 100,
+    backgroundColor: 'blue',
+    borderWidth: 10,
+    borderColor: 'red',
+  },
+  item2: {
+    flex: 2,
+    height: 100,
+    backgroundColor: 'green',
+  },
+  item3_1: {
+    flex:1,
+    //height: 100,
+    backgroundColor: 'tomato',
+  },
+  item3_2: {
+    flex:2,
+    //height: 100,
+    backgroundColor: 'springgreen',
+  },
+  item3_3: {
+    flex:2,
+    //height: 100,
+    backgroundColor: 'peru',
+  },
+  item3_4: {
+    flex:1,
+    //height: 100,
+    backgroundColor: 'olive',
+  },
+  item4: {
+    flex: 2,
+    height: 100,
+    backgroundColor: 'purple',
+  },
+  item5: {
+    flex: 1,
+    height: 100,
+    backgroundColor: 'olive',
   },
 });
